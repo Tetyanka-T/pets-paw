ff514dc9716e137c80c9473c3d9b5f667ecf6d92
Tatyana Tupalo <tatyana080489t@gmail.com>
2022-07-04 10:50:01 +0300

    add components

---
+++ src/components/PanelVoting/PanelVoting.jsx
@@ -0,0 +1,34 @@
+import { ReactComponent as LikeTransp } from '../../image/like-transp.svg';
+import { ReactComponent as HartTransp } from '../../image/hart-transp.svg';
+import { ReactComponent as DisLikeTransp } from '../../image/dislike-transp.svg';
+import { ReactComponent as Like } from '../../image/like-green-big.svg';
+import { ReactComponent as Hart } from '../../image/hart-white-big.svg';
+import { ReactComponent as DisLike } from '../../image/dislike-yellow-big.svg';
+import s from './PanelVoting.module.scss';
+
+const PanelVoting = () => {
+  return (
+    <ul className={s.panelVoting_list}>
+      <li className={s.panelVoting_item}>
+        <button className={s.panelVoting_buttonLike}>
+          <LikeTransp className={s.icon} />
+          <Like className={s.icon_hover} />
+        </button>
+      </li>
+      <li className={s.panelVoting_item}>
+        <button className={s.panelVoting_buttonHart}>
+          <HartTransp className={s.icon} />
+          <Hart className={s.icon_hover} />
+        </button>
+      </li>
+      <li className={s.panelVoting_item}>
+        <button className={s.panelVoting_buttonDisLike}>
+          <DisLikeTransp className={s.icon} />
+          <DisLike className={s.icon_hover} />
+        </button>
+      </li>
+    </ul>
+  );
+};
+
+export default PanelVoting;
---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,27 @@
+import { ReactComponent as Search } from '../../image/search.svg';
+import s from './SearchForm.module.scss';
+
+const SearchForm = ({ onSearch }) => {
+  const handleSearch = e => {
+    e.preventDefault();
+    onSearch(e.target.elements.searchImage.value);
+  };
+
+  return (
+    <form onSubmit={handleSearch} className={s.searchForm}>
+      <input
+        className={s.searchForm_input}
+        type="text"
+        name="searchImage"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search for breeds by name"
+      />
+      <button type="submit" className={s.button}>
+        <Search />
+      </button>
+    </form>
+  );
+};
+
+export default SearchForm;
