3a98ba45e02187a8c9c6089e6fc870806dcba9db
Tatyana Tupalo <tatyana080489t@gmail.com>
2022-08-13 16:39:00 +0300

    fix comeBack button

--- src/components/ComeBack/ComeBackButton.jsx
+++ src/components/ComeBack/ComeBackButton.jsx
@@ -1,17 +1,15 @@
 import { ReactComponent as ComeBack } from '../../image/come-back.svg';
-
+import { useNavigate } from 'react-router-dom';
 import s from './ComeBackButton.module.scss';
 
 const ComeBackButton = () => {
-  const goBack = () => {
-    window.history.back();
-  };
+  const navigate = useNavigate();
 
   return (
     <button
       type="button"
       className={s.galleryButton_comeBack}
-      onClick={() => goBack()}
+      onClick={() => navigate(-1)}
     >
       <ComeBack />
     </button>
